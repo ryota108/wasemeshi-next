ebaa571ddf4df9aa9641aee9961db841fa18acc1
Ryota Kodama <aya.ryo.718@outlook.jp>
2022-06-14 11:57:36 +0900

    依存関係の修正

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,4 +1,4 @@
-import fetch from "../../../node_modules/node-fetch/src/index";
+import fetch from "node-fetch";
 
 const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&keyword=高田馬場`;
 
@@ -42,9 +42,3 @@ export default async (req, res) => {
   const result = await fetch(url);
   res.json(result.body);
 };
-export  function getStaticProps() {
-  fetch
-  return {
-    props: {}
-  }
-}

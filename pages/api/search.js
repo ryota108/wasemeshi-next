84032526ba2b3cc4aff207e6d7594fd52692c662
Ryota <aya.ryo.718@outlook.jp>
2023-05-10 17:23:19 +0900

    検索機能の修正とリファクタリング

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,41 +3,38 @@ import fetch from "node-fetch";
 const defaultEndpoint = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&format=json&keyword=高田馬場`;
 
 const handler = async (req, res) => {
+  const queryParams = {
+    keyword: req.query.keyword,
+    start: req.query.start,
+    oneCoin: req.query.oneCoin === "true",
+    twoCoin: req.query.twoCoin === "true",
+    oneBill: req.query.oneBill === "true",
+    twoBill: req.query.twoBill === "true",
+    manyBill: req.query.manyBill === "true",
+    noodle: req.query.noodle === "true",
+    meat: req.query.meat === "true",
+    cafe: req.query.cafe === "true",
+  };
+
+  const queryMapping = {
+    keyword: `&keyword=${req.query.keyword}`,
+    start: `&start=${req.query.start}`,
+    oneCoin: "&budget=B009,B010",
+    twoCoin: "&budget=B001,B011&count=70",
+    oneBill: "&budget=B002&count=100",
+    twoBill: "&budget=B003&count=55",
+    manyBill: "&budget=B008,B004,B005&count=20",
+    noodle: "&genre=G013",
+    meat: "&genre=G008&count=20",
+    cafe: "&genre=G014&count=15",
+  };
+
   let url = defaultEndpoint;
 
-  switch (true) {
-    case typeof req.query.keyword !== "undefined":
-      url = `${url}&keyword=${req.query.keyword}`;
-      break;
-    case typeof req.query.start !== "undefined":
-      url = `${url}&start=${req.query.start}`;
-      break;
-    case req.query.oneCoin === "true":
-      url = `${url}&budget=B009,B010`;
-      break;
-    case req.query.twoCoin === "true":
-      url = `${url}&budget=B001,B011&count=70`;
-      break;
-    case req.query.oneBill === "true":
-      url = `${url}&budget=B002&count=100`;
-      break;
-    case req.query.twoBill === "true":
-      url = `${url}&budget=B003&count=55`;
-      break;
-    case req.query.manyBill === "true":
-      url = `${url}&budget=B008,B004,B005&count=20`;
-      break;
-    case req.query.noodle === "true":
-      url = `${url}&genre=G013`;
-      break;
-    case req.query.meat === "true":
-      url = `${url}&genre=G008&count=40`;
-      break;
-    case req.query.cafe === "true":
-      url = `${url}&genre=G014&count=15`;
-      break;
-    case req.query.cafe === "true":
-      url = `${url}&genre=G007,G017&count=35`;
+  for (const param in queryParams) {
+    if (queryParams[param] && queryMapping[param]) {
+      url += queryMapping[param];
+    }
   }
 
   url = encodeURI(url);
@@ -45,5 +42,4 @@ const handler = async (req, res) => {
   const result = await fetch(url);
   res.json(result.body);
 };
-
 export default handler;

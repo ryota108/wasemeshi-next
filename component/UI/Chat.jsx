638b9e8528b57f67cbf1e935343e0a030277c8ad
Ryota <aya.ryo.718@outlook.jp>
2023-05-20 17:00:22 +0900

    ChatのUIコンポーネントの作成

---
+++ component/UI/Chat.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import classes from "../../styles/Chat.module.css";
+
+const Chat = ({ info, isShop = false, shopImg }) => {
+  const longInfo = info.length > 15;
+  return (
+    <>
+      {isShop ? (
+        <>
+          <div className={classes.shopIcon}>
+            <img className={classes.logoImg} src={shopImg} />
+          </div>
+          <div
+            className={`${classes.myCommentBudget} ${
+              longInfo ? classes.longInfo : ""
+            }`}
+          >
+            <p className={classes.budgetText}>{info}</p>
+          </div>
+        </>
+      ) : (
+        <div className={classes.balloon1}>
+          <div className={classes.faceicon}>
+            <img src="/images/logoCopy-min.png" />
+          </div>
+          <div className={classes.chatting}>
+            <div className={classes.says}>
+              <p className={classes.budgetInfo}>{info}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Chat;

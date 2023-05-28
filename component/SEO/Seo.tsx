a557e55d28575ca54b24ba408333483201ff9228
Ryota <aya.ryo.718@outlook.jp>
2023-05-28 12:33:16 +0900

    twitterカードの動的に変更

--- component/SEO/Seo.tsx
+++ component/SEO/Seo.tsx
@@ -41,8 +41,9 @@ const Seo: React.FC<Props> = ({
       <meta property="og:image:width" content={String(imgWidth)} />
       <meta property="og:image:height" content={String(imgHeight)} />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content="カードのタイトル" />
-      <meta name="twitter:description" content="カードの説明" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imgUrl}/>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;display=swap"

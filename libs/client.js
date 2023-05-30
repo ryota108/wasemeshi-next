147b1529f15bce98edd966d2afffe1a40b5f7ad5
Ryota <aya.ryo.718@outlook.jp>
2023-05-30 21:27:37 +0900

    microCMS APIkey 秘匿化

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'wasemeshi',
-  apiKey: "mRTocAzBvqncBo9rKeVGddkWiqoMqpisVsmJ",
+  apiKey: process.env.CMS_API_KEY,
 });

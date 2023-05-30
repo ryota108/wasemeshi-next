64c69e0a886caf2297485f8db698dee4611025b3
Ryota <aya.ryo.718@outlook.jp>
2023-05-30 21:31:47 +0900

    Revert "microCMS APIkey 秘匿化"
    This reverts commit 147b1529f15bce98edd966d2afffe1a40b5f7ad5.

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'wasemeshi',
-  apiKey: process.env.CMS_API_KEY,
+  apiKey: "mRTocAzBvqncBo9rKeVGddkWiqoMqpisVsmJ",
 });
